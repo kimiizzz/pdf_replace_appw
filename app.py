b4878ff76fcd209f1074cd10427ca9c87fbda813
kimiizzz <luqman.azhar@my.panasonic.com>
2025-10-04 15:59:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,6 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # Render requires listening on 0.0.0.0:5000
+    app.run(host="0.0.0.0", port=5000)
+
